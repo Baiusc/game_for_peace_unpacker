38c2f730717860843dce19423eb8fa95e8b001ed
baizs_work_pc_ubuntu_kioxia <zhongshan.bai@vitalchem.com>
2025-09-19 17:16:26 +0800

    feat: 在安卓上运行akmod提示权限不够，需要ROOT环境，小米手机ROOT太麻烦，考虑电脑用QEMU模拟ARM64环境跑AKMOD。目前卡在QEMU虚拟ARM64安装linux系统遇到BUG失败

--- python/bms_linux.py
+++ python/bms_linux.py
@@ -2,7 +2,7 @@
 Author       : baizs_work_pc_ubuntu_kioxia zhongshan.bai@vitalchem.com
 Date         : 2025-08-21 10:10:50
 LastEditors  : baizs_work_pc_ubuntu_kioxia zhongshan.bai@vitalchem.com
-LastEditTime : 2025-09-19 13:33:15
+LastEditTime : 2025-09-19 16:19:32
 FilePath     : /game_for_peace_unpacker/python/bms_linux.py
 Description  : 
 
@@ -15,7 +15,7 @@ from glob import glob
 
 
 # pak_file = "./paks/game_patch_1.32.11.14059.pak"
-pak_file = "./paks/map_lobby_1.32.11.13800_0.67.pak"
+pak_file = "./paks/game_patch_1.33.12.14267.pak"
 
 # pak_file = "./paks/map_lobby_1.33.12.14210.pak"
 # pak_file = "./paks/map_weapon_1.33.12.14210.pak"
