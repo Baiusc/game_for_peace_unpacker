e2dad03d3e03de3576c9191cde23bd91f946fc7c
baizs_work_pc_ubuntu_kioxia <zhongshan.bai@vitalchem.com>
2025-10-21 20:28:45 +0800

    feat: 发现dat打包14343枪补就没问题，而打包14340天线pak就会大小不等

--- py_script/检查新旧dat名称大小是否全等.py
+++ py_script/检查新旧dat名称大小是否全等.py
@@ -141,10 +141,10 @@ if __name__ == "__main__":
     is_auto_rename = False # 设置为 True 启用自动重命名，设置为 False 仅进行检查
 
     # my_dir = "./release/RE枪补V5发布20250930/my_dat20251013"
-    # my_dir = "./release/RE枪补V5发布20250930/1020新增"
-    my_dir = "./release//RE范围V1发布20251014/待打包枪补+子弹模板dat"
-    src_dir = "./release/RE枪补V5发布20250930/my_dat20251013"
-    # src_dir = "./paks/dat_temp"
+    my_dir = "./release/RE天线V4发布20250930"
+    # my_dir = "./release//RE范围V1发布20251014/待打包枪补+子弹模板dat"
+    # src_dir = "./release/RE枪补V5发布20250930/my_dat20251013"
+    src_dir = "./paks/dat_temp"
     # src_dir = "./paks/dat_patch_14327原厂"
     # src_dir = "./paks/dat_map_weapon14210原厂"
     # src_dir = "./paks/ShadowTrackerExtra_patch_14343原厂"
--- python/bms_linux.py
+++ python/bms_linux.py
@@ -2,7 +2,7 @@
 Author       : baizs_work_pc_ubuntu_kioxia zhongshan.bai@vitalchem.com
 Date         : 2025-08-21 10:10:50
 LastEditors  : baizs_work_pc_ubuntu_kioxia zhongshan.bai@vitalchem.com
-LastEditTime : 2025-10-20 13:42:17
+LastEditTime : 2025-10-21 20:27:51
 FilePath     : /game_for_peace_unpacker/python/bms_linux.py
 Description  : 
 
@@ -14,9 +14,9 @@ import time
 from glob import glob
 
 
-pak_file = "./paks/game_patch_1.33.12.14343原厂（复件）.pak"
+# pak_file = "./paks/game_patch_1.33.12.14343原厂（复件）.pak"
 # pak_file = "./paks/map_weapon_1.33.12.14210原厂（复件）.pak"
-# pak_file = "./paks/game_patch_1.33.12.14266.pak"
+pak_file = "./paks/game_patch_1.33.12.14343原厂（复件）.pak"
 dat_dir = "./paks/dat_temp"
 
 # 定义解包函数dat_temp
